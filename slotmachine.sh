165a6d3463e93dc8f9df75ebf53e1a6801da8e9e
cthacker-udel <cthacker@udel>
2021-04-19 17:35:19 -0400

    Quick Fix: Fixed bug in slotmachine.sh with ending quote

--- slotmachine.sh
+++ slotmachine.sh
@@ -27,10 +27,24 @@ while true; do
         randomnumber=$(shuf -i 1-7 -n 1)
         number="${numbers}${randomnumber}"
         echo "$randomnumber"
-        letter1=${randomnumber:0:1}
-        letter2=${randomnumber:1:1}
-        letter3=${randomnumber:2:1}
+        letter1=${number:0:1}
+        letter2=${number:1:1}
+        letter3=${number:2:1}
         echo "Letter 1 : $letter1, Letter 2 : $letter2, Letter 3 : $letter3"
+        if [ "$letter1" == "$letter2" -a "$letter1" == "$letter3" -a "$letter1" -eq 7 ]; then
+            echo "Congratulations! You won 10000!"
+            amount=$((amount+10000))
+        elif [ "$letter1" == "$letter2" ]; then
+            echo "Congratulations! You won 100"
+            amount=$((amount+100))
+        elif [ "$letter1" == "$letter3" ]; then
+            echo "Congratulations! You won 100"
+            amount=$((amount+100))
+        elif [ "$letter2" == "$letter3" ]; then
+            echo "Congratulations! You won"
+        else
+            echo "next round"
+        fi
     fi
 done
 
