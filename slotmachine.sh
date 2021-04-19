83cff4f011ac253b247bf1fcf4e163d39ffaa181
cthacker-udel <cthacker@udel>
2021-04-19 17:50:53 -0400

    Added functionality to slotmachine.sh, add amount if amount is 0, and also added functionality to wager amounts as well

--- slotmachine.sh
+++ slotmachine.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Welcome to the Thacker Casino! - This is the slot machine program"
+echo -e "Welcome to the Thacker Casino! - This is the slot machine program\n"
 
 
-read -p "Enter the amount to start with" amount
+read -p "Enter the amount to start with      " amount
 
 
 while true; do
@@ -13,6 +13,22 @@ while true; do
         echo -e "\nYour amount is : $amount"
         exit 0
     else
+        if [ "$amount" -le 0 ]; then
+            read -p "Enter the amount of money to add to your account     " addamount
+            amount=$((addamount + amount))
+            echo -e "\nAdded $addamount to account, total balance is : $amount\n"
+        fi
+        echo -e "\n"
+        while true; do
+            read -p "Enter the wager    " wager
+            if [ "$wager" -gt "$amount" ]; then
+                echo -e "\nWager is greater than amount, enter an amount less than or equal to amount\n"
+            else
+                echo -e "\nWager is : $wager\n"
+                break
+            fi
+        done
+        amount=$((amount-wager))
         echo -e "Spinning\n"
         numbers=""
         sleep 1
@@ -30,19 +46,29 @@ while true; do
         letter1=${number:0:1}
         letter2=${number:1:1}
         letter3=${number:2:1}
-        echo "Letter 1 : $letter1, Letter 2 : $letter2, Letter 3 : $letter3"
+        #echo "Letter 1 : $letter1, Letter 2 : $letter2, Letter 3 : $letter3"
         if [ "$letter1" == "$letter2" -a "$letter1" == "$letter3" -a "$letter1" -eq 7 ]; then
-            echo "Congratulations! You won 10000!"
-            amount=$((amount+10000))
+            amountwon=$((wager * 10000))
+            echo "Congratulations! You won $amountwon!"
+            amount=$((amount+amountwon))
+            echo -e "\nYour amount : $amount\n"
         elif [ "$letter1" == "$letter2" ]; then
-            echo "Congratulations! You won 100"
-            amount=$((amount+100))
+            amountwon=$((wager * 100))
+            echo "Congratulations! You won $amountwon"
+            amount=$((amount+amountwon))
+            echo -e "\nYour amount : $amount\n"
         elif [ "$letter1" == "$letter3" ]; then
-            echo "Congratulations! You won 100"
-            amount=$((amount+100))
+            amountwon=$((wager * 100))
+            echo "Congratulations! You won $amountwon"
+            amount=$((amount+amountwon))
+            echo -e "\nYour amount : $amount\n"
         elif [ "$letter2" == "$letter3" ]; then
-            echo "Congratulations! You won"
+            amountwon=$((wager * 100))
+            echo "Congratulations! You won $amountwon"
+            amount=$((amount+amountwon))
+            echo -e "\nYour amount : $amount\n"
         else
+            echo -e "\nYour amount : $amount\n"
             echo "next round"
         fi
     fi
